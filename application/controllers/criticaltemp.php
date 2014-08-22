a42f87139fa52acec73b041ea49a41a433896079
unknown <nattaponwor@CPR_nattaponwor.rtl.cp.co.th>
2014-08-22 17:24:52 +0700

    edit critical page but do not successful
    ps. waiting for update

--- application/controllers/criticaltemp.php
+++ application/controllers/criticaltemp.php
@@ -1,23 +1,33 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
 class Criticaltemp extends CI_Controller {
-    public function __construct(){
-        parent::__construct();
-		$this->session->set_userdata('session_page', 'criticaltemp');
-        $this->load->model("criticaltemp_model");  
-    }
-    
-    public function index() {
+	public function __construct() {
+		parent::__construct();
+		$this -> session -> set_userdata('session_page', 'criticaltemp');
+		$this -> load -> model("criticaltemp_model");
+	}
+
+	public function index() {
 		$cookie = get_cookie('username_cookie');
-		if( $cookie != null) {
-        	$this->view->page_view("criticaltemp_view");
+		if ($cookie != null) {
+			$showTable["id"] = $this -> criticaltemp_model -> showtable();
+			$showTable["sorttimeDESC"] = $this -> criticaltemp_model -> sortingDESC();
+			$showTable["sorttimeASC"] = $this -> criticaltemp_model -> sortingASC();
+			$this->view->p($this -> criticaltemp_model -> sortingDESC());
+			$this->view->p($this -> criticaltemp_model -> sortingASC());
+			$this -> view -> page_view("criticaltemp_view", $showTable);
 		} else {
 			redirect('/login/', 'refresh');
-		}  
-    }
-   
-    public function show( $in, $type, $list) {
-        $showTable["id"] = $this->criticaltemp_model->showtable();  
-        $this->view->page_view("criticaltemp_view", $showTable);
-    }
+		}
+	}
+
+	public function show($in, $type, $list) {
+		$showTable["id"] = $this -> criticaltemp_model -> showtable();
+		$showTable["sorttimeDESC"] = $this -> criticaltemp_model -> sortingDESC();
+		$showTable["sorttimeASC"] = $this -> criticaltemp_model -> sortingASC();
+		$this -> view -> page_view("criticaltemp_view", $showTable);
+	}
+
 }
--- application/models/criticaltemp_model.php
+++ application/models/criticaltemp_model.php
@@ -1,26 +1,60 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
 class Criticaltemp_model extends CI_model {
 
-    function __construct() {
-        parent::__construct();
-        $this->load->database();
-    }     
-    
-    function showtable() {
-        $this->db->select('asset.store_id, asset.store_name, asset.asset_barcode, asset_shortname, asset_type.shortcode, asset_type.type, temperature.temp, 
-        				   temperature.status');
-        $this->db->from('asset');
-        $this->db->join('temperature', 'temperature.asset_id = asset.id');
-        $this->db->join('asset_type', 'asset_type.id = asset.asset_typeid');
-		$this->db->where('temperature.status', 'ALERT');
-        //$this->db->order_by('temperature.id', 'DESC');
-		//$this->db->limit(10);
-        $query = $this->db->get();
-        $assets = array();                       
-        foreach ($query->result_array() as $row) {
-            $assets[] = $row;
-        }
-        return $assets;
+	function __construct() {
+		parent::__construct();
+		$this -> load -> database();
 	}
+
+	function showtable() {
+		$this -> db -> select('asset.store_id, asset.store_name, asset.asset_barcode, asset_shortname, asset_type.shortcode, asset_type.type, temperature.temp, 
+        				   temperature.status, temperature.time');
+		$this -> db -> from('asset');
+		$this -> db -> join('temperature', 'temperature.asset_id = asset.id');
+		$this -> db -> join('asset_type', 'asset_type.id = asset.asset_typeid');
+		$this -> db -> where('temperature.status', 'ALERT');
+		$this -> db -> or_where('temperature.status', 'WAIT');
+		$this -> db -> order_by('temperature.id', 'DESC');
+		//$this->db->limit(10);
+		$query = $this -> db -> get();
+		$assets = array();
+		foreach ($query->result_array() as $row) {
+			$assets[] = $row;
+		}
+		return $assets;
+	}
+
+	function sortingDESC() {
+		$this -> db -> select('asset.id, temperature.status, temperature.time');
+		$this -> db -> from('asset');
+		$this -> db -> join('temperature', 'temperature.asset_id = asset.id');
+		$this -> db -> where('temperature.status', 'ALERT');
+		$this -> db -> or_where('temperature.status', 'WAIT');
+		$this -> db -> order_by('temperature.time', 'DESC');
+		$query = $this -> db -> get(); 
+		$assets = array();
+		foreach ($query->result_array() as $row) {
+			$assets[$row['id']] = $row;
+		}
+		return $assets;
+	}
+
+	function sortingASC() {
+		$this -> db -> select('asset.id, temperature.status, temperature.time');
+		$this -> db -> from('asset');
+		$this -> db -> join('temperature', 'temperature.asset_id = asset.id');
+		$this -> db -> where('temperature.status', 'ALERT');
+		$this -> db -> or_where('temperature.status', 'WAIT');
+		$this -> db -> order_by('temperature.time', 'ASC');
+		$query = $this -> db -> get(); 
+		$assets = array();
+		foreach ($query->result_array() as $row) {
+			$assets[$row['id']] = $row;
+		}
+		return $assets;
+	}
+
 }
